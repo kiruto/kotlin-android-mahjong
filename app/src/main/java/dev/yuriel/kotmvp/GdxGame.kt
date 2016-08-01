2049f124604c75df007ffcc8dca6492f9c72efff
yuriel <yuriel3183@gmail.com>
2016-08-02 00:27:09 +0800

    add resources

--- app/src/main/java/dev/yuriel/kotmvp/GdxGame.kt
+++ app/src/main/java/dev/yuriel/kotmvp/GdxGame.kt
@@ -10,19 +10,18 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch
  * Created by yuriel on 8/1/16.
  */
 class GdxGame: ApplicationAdapter() {
-    var batch: SpriteBatch? = null
-    var img: Texture? = null
+    val batch: SpriteBatch by lazy { SpriteBatch() }
+    val img: Texture by lazy { Texture("ji1.png") }
 
     override fun create() {
-        batch = SpriteBatch()
-        img = Texture("ji7-66-90-s.png")
+
     }
 
     override fun render() {
         Gdx.gl.glClearColor(1F, 0F, 0F, 1F)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch?.begin()
-        batch?.draw(img, 0F, 0F)
-        batch?.end()
+        batch.begin()
+        batch.draw(img, 0F, 0F)
+        batch.end()
     }
 }
